c5434964abd4f74c0e0e4a365d52dbfac5e41070
Jumeb <bricejume@gmail.com>
2022-07-05 11:45:42 +0100

    Scafolded the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'actor': ["Actor"],
+        'inter': ["Inter"],
+        'arial': ["Arial"],
+        'calibri': ["Calibri"]
+      },
+      colors: {
+        'eco-green': {
+          DEFAULT: '#16FD0B',
+        },
+        'eco-white': {
+          DEFAULT: '#FFFFFF',
+        },
+        'eco-dark': {
+          DEFAULT: '#000044'
+        },
+        'eco-purple': {
+          DEFAULT: '#0000DE'
+        }
+      }
+    },
   },
   plugins: [],
 }
